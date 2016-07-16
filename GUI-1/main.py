0f0b7581a002e27127dbbe38ab5b31d5b24a1222
William Zhu <williamzhu@ipsec-143-215-25-157.vpn.gatech.edu>
2016-07-16 21:58:42 +0800

    Added Signal file -> 'main'

--- GUI-1/main.py
+++ GUI-1/main.py
@@ -1,2 +1,25 @@
-from PyQt5 import QtGui
+from PyQt5 import QtGui, QtCore, QtWidgets
 import sys
+import gui1
+class Signals(QtWidgets.QMainWindow, gui1.Ui_MainWindow):
+    def __init__(self, parent=None):
+        super(Signals, self).__init__(parent)
+        self.setupUi(self)
+
+
+
+
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)  # A new instance of QApplication
+    form = Signals()                 # We set the form to be our ExampleApp (design)
+    form.show()                         # Show the form
+    app.exec_()                         # and execute the app
+
+
+
+
+
+
+if __name__ == '__main__':              # if we're running file directly and not importing it
+    main()                              # run the main function
